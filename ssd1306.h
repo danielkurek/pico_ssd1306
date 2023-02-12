7283b5cd15522778b64a20261dc476432fe29cae
Daniel Kurek <daniel.kurek.dev@gmail.com>
2023-02-12 18:37:28 +0100

    Fix for CPP compilation

--- ssd1306.h
+++ ssd1306.h
@@ -30,6 +30,10 @@ SOFTWARE.
 
 #ifndef _inc_ssd1306
 #define _inc_ssd1306
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <pico/stdlib.h>
 #include <hardware/i2c.h>
 
@@ -250,5 +254,9 @@ void ssd1306_draw_string_with_font(ssd1306_t *p, uint32_t x, uint32_t y, uint32_
 	@param[in] s : text to draw
 */
 void ssd1306_draw_string(ssd1306_t *p, uint32_t x, uint32_t y, uint32_t scale, const char *s);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
